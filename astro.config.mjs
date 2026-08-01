6f6d844149f0c7fa070a28b35718016223fcb2b3
Daniel Garcia <daniel.w.garcia@outlook.com>
2026-08-01 12:30:10 -0700

    Firts draft functioning site

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,3 +1,15 @@
+// astro.config.mjs
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
+import robotsTxt from 'astro-robots-txt';
 
-export default defineConfig({});
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://www.ipostdeals.com',
+  integrations: [
+    sitemap(),
+    robotsTxt({
+      sitemap: true,
+    }),
+  ],
+});
